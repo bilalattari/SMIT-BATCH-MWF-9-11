516a05e123449e41586dfa6b5482774722dc90ba
Bilal raza <attari1235@gmail.com>
2023-08-02 14:03:32 +0500

    Javascript Variables and Hoisting Concept

---
+++ Javascript/concepts/hoisting.js
@@ -0,0 +1,21 @@
+// In JavaScript, Hoisting is the default behavior of
+// moving all the declarations at the top of the scope
+// before code execution.
+// var hello;
+// var username;
+console.log("saylani")
+
+
+// there are two things in variable
+// 1.Variable declaration
+// 2.Assign Value to variable
+var hello = 'Hello World'
+var username = 'bilal raza'
+var saylani = 'saylani'
+
+// if you want to update the variable you declared before so
+// just put the name of variable without var keyword and updated value
+username = true //Boolean
+// console.log(username)
+username = 12134 //number
+console.log(username)
