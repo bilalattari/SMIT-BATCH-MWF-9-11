f742363d9c7f92212f35c3a2ae36be68163df0a2
Bilal raza <attari1235@gmail.com>
2023-08-04 10:54:59 +0500

    Hoisting

--- Javascript/concepts/hoisting.js
+++ Javascript/concepts/hoisting.js
@@ -1,15 +1,29 @@
 // In JavaScript, Hoisting is the default behavior of
 // moving all the declarations at the top of the scope
 // before code execution.
+
 // var hello;
 // var username;
-console.log("saylani")
-
 
 // there are two things in variable
 // 1.Variable declaration
 // 2.Assign Value to variable
-var hello = 'Hello World'
+
+var hello = helloWorld
+
+console.log(hello)
+
+var helloWorld = 'Hello World'
+hello = helloWorld
+
+console.log(hello)
+
+//a : empty in both
+//b : undefined in 17 and hello world in 21
+//c : undefined in 17 and 19
+//d : error in line number 15
+//e : emmpty in 17 and helloworld in 21
+
 var username = 'bilal raza'
 var saylani = 'saylani'
 
@@ -18,4 +32,34 @@ var saylani = 'saylani'
 username = true //Boolean
 // console.log(username)
 username = 12134 //number
-console.log(username)
+// console.log(username)
+
+// variables declared using let cannot be redeclared
+
+let classroomm = 'BATCH 9'
+classroomm = 'BATCH 10'
+
+console.log(classroomm)
+
+// variables declared using let cannot be used before they are declared
+
+// var before = after
+// var after = 'After variable'
+
+let after = 'After variable'
+let before = after
+
+console.log(before)
+
+// var has functional scope and let have block scope
+
+{
+  let parent = 'parent'
+  console.log(parent)
+  console.log(child)
+  {
+    let child = 'child'
+    console.log(parent)
+    console.log(child)
+  }
+}
