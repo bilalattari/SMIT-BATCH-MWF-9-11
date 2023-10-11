87f64874268fb995728c60a1a4fb3fac275820e9
bilalattari <attari1235@gmail.com>
2023-10-11 10:34:43 +0500

    Todo App with local Storage

--- Javascript/localstorage/index.js
+++ Javascript/localstorage/index.js
@@ -22,23 +22,63 @@ console.log('localStorage->', localStorage)
 // var username = 'bilal'
 // localStorage.setItem('username', username)
 
-const upate_username_btn = document.getElementById('upate_username_btn')
-const username_inp = document.getElementById('user_name_inp')
-const username_txt = document.getElementById('user_name')
+const add_todo_btn = document.getElementById('upate_username_btn')
+const todo_inp = document.getElementById('user_name_inp')
+const todo_container = document.getElementById('todo_container')
 
-upate_username_btn.addEventListener('click', updateUserName)
+const todos = JSON.parse(localStorage.getItem('todos')) || []
+console.log('todos=>', todos)
 
-function updateUserName () {
-  localStorage.setItem('username', username_inp.value)
-  setUserName()
-  username_inp.value = ''
-}
-function setUserName () {
-  const username = localStorage.getItem('username')
-  username_txt.innerText = username || '--'
+for (var i = 0; i < todos.length; i++) {
+  addTodo(todos[i])
 }
 
-setUserName()
+add_todo_btn.addEventListener('click', function () {
+  addTodo()
+})
+
+function addTodo (txt) {
+  const div = document.createElement('div')
+  div.className = 'todo-div'
+
+  const todoTxt = document.createElement('span')
+  div.className = 'todo-text'
+  todoTxt.innerText = txt ? txt : todo_inp.value
+
+  const delBtn = document.createElement('button')
+  delBtn.innerText = 'Delete'
+  delBtn.onclick = function () {
+    div.remove()
+  }
+
+  div.appendChild(todoTxt)
+  div.appendChild(delBtn)
+
+  todo_container.appendChild(div)
+  if (!txt) {
+    addTodoToStorage(todo_inp.value)
+    todo_inp.value = ''
+  }
+}
+
+function addTodoToStorage (txt) {
+  todos.push(txt)
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// upate_username_btn.addEventListener('click', updateUserName)
+
+// function updateUserName () {
+//   localStorage.setItem('username', username_inp.value)
+//   setUserName()
+//   username_inp.value = ''
+// }
+// function setUserName () {
+//   const username = localStorage.getItem('username')
+//   username_txt.innerText = username || '--'
+// }
+
+// setUserName()
 
 // var usernameFromLs = localStorage.getItem('username')
 // console.log('usernameFromLs->', usernameFromLs)
